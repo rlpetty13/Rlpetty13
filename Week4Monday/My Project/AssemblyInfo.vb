9f6fcfc9737f16fcef38955fa38a09626cfad9bd
rlpetty13 <rlpetty13@live.com>
2015-10-14 11:44:25 -0700

    Added more files from todays class.

---
+++ Week4Monday/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("Week4Monday")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Bellevue College")> 
+<Assembly: AssemblyProduct("Week4Monday")> 
+<Assembly: AssemblyCopyright("Copyright © Bellevue College 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("18c85260-578b-46f5-83e2-76b6542cbc0d")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
