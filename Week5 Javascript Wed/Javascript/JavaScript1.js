67f519b363161b891315d5d19beb5984d0f91657
rlpetty13 <rlpetty13@live.com>
2015-10-26 12:11:53 -0700

    New monday update

---
+++ Week5 Javascript Wed/Javascript/JavaScript1.js
@@ -0,0 +1,10 @@
+﻿//working with arrays
+/*
+var months = ["Jan", "Feb", "March", "April", "May", "June", "July", "August", "Septemer", "October", "November", "December"];
+document.getElementById(demo);
+
+var currentDate = new Date();
+console.log(currentDate.getMonth());
+
+*/
+
---
+++ Week5 Javascript Wed/Javascript/JavaScript2.js
@@ -0,0 +1,20 @@
+﻿//var myBase = 5;
+var result='';
+var divResult = document.getElementById("result");
+
+createMultTable(10, 15);
+
+//create function
+
+function createMultTable(myBase, max) {
+
+    var l_base = myBase;
+    result = "<h1> Using innerHTML </h1>";
+    for (i = 0; i <= max; i++)
+
+    {  result += "<p>" + i + "<span class='symbols'> * </span>" + l_base + "<span class='symboles'> = </span>" +i * l_base + "</p>";   }
+
+}
+
+
+divResult.innerHTML += result;
