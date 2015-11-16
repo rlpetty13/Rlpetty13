da8b232803c4df487c25a3a6b2038b4a3b416500
rlpetty13 <rlpetty13@live.com>
2015-11-16 11:30:23 -0800

    Created Rhombus

---
+++ Week5 Javascript Wed/JavaScript1.js
@@ -0,0 +1,128 @@
+﻿      
+function displayRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+    upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+    downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+   upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+    downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+}
+
+
+function upRight(pHeight, pColorOdd, pColorEven, pSymbol) {
+
+    var rLine = "";
+
+    for (i = 0; i < pHeight; i++) {
+
+        rLine += "<p>";
+        
+        for (j = 0; j <= i; j++) {
+
+            if (j % 2)
+
+                //even 
+
+                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+
+            else 
+                //odd
+
+                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+
+        }
+
+        rLine += "</p>"
+       
+    }
+
+    document.getElementById("upRight").innerHTML = rLine;
+}
+
+function downRight(pHeight, pColorOdd, pColorEven, pSymbol) {
+
+    var rLine = "";
+
+    for (i = pHeight; i > 0; i--) {
+
+        rLine += "<p>";
+
+        for (j = 0; j < i; j++) {
+
+            if (j % 2)
+
+                //even 
+
+                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+
+            else
+                //odd
+
+                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+
+        }
+
+        rLine += "</p>"
+
+    }
+
+    document.getElementById("downRight").innerHTML = rLine;
+}
+
+function upLeft(pHeight, pColorOdd, pColorEven, pSymbol) {
+
+    var rLine = "";
+
+    for (i = 0; i < pHeight; i++) {
+
+        rLine += "<p>";
+
+        for (j = 0; j <= i; j++) {
+
+            if (j % 2)
+
+                //even 
+
+                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+
+            else
+                //odd
+
+                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+
+        }
+
+        rLine += "</p>"
+
+    }
+
+    document.getElementById("upLeft").innerHTML = rLine;
+}
+
+function downLeft(pHeight, pColorOdd, pColorEven, pSymbol) {
+
+    var rLine = "";
+
+    for (i = pHeight; i > 0; i--) {
+
+        rLine += "<p>";
+
+        for (j = 0; j < i; j++) {
+
+            if (j % 2)
+
+                //even 
+
+                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+
+            else
+                //odd
+
+                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+
+        }
+
+        rLine += "</p>"
+
+    }
+
+    document.getElementById("downLeft").innerHTML = rLine;
+}
